b8bd27aa427ab20431d5d5cbc736da012ffea415
yorsyboy <adekunletaiwo43@yahoo.com>
2022-04-04 13:57:34 +0100

    ADD MOBILE MENU

---
+++ main.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById('myNav').style.display = 'block';
+}
+
+function closeNav() {
+  document.getElementById('myNav').style.display = 'none';
+}
+
+document.querySelector('.toggle').addEventListener('click', openNav);
+document.querySelector('.closebtn').addEventListener('click', closeNav);
+document.querySelectorAll('.pop-nav').forEach((link) => {
+  link.addEventListener('click', () => {
+    document.getElementById('myNav').style.display = 'none';
+  });
+});
